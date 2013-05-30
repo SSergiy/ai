c018d71294a2f741265ed5cf79af91c83469ead9
Darius <damu@akra.de>
2013-05-30 19:22:03 +0200

    Add GUI

---
+++ HES-Konsole/GUI/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InterfaceName = new System.Windows.Forms.TextBox();
+            this.MethodenName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Senden = new System.Windows.Forms.Button();
+            this.AnzahlAufrufe = new System.Windows.Forms.TextBox();
+            this.Empfangen = new System.Windows.Forms.Button();
+            this.client = new System.Windows.Forms.TextBox();
+            this.clientname = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Interface";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Methode";
+            // 
+            // InterfaceName
+            // 
+            this.InterfaceName.Location = new System.Drawing.Point(15, 25);
+            this.InterfaceName.Name = "InterfaceName";
+            this.InterfaceName.Size = new System.Drawing.Size(100, 20);
+            this.InterfaceName.TabIndex = 2;
+            // 
+            // MethodenName
+            // 
+            this.MethodenName.Location = new System.Drawing.Point(15, 65);
+            this.MethodenName.Name = "MethodenName";
+            this.MethodenName.Size = new System.Drawing.Size(100, 20);
+            this.MethodenName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Anzahl";
+            // 
+            // Senden
+            // 
+            this.Senden.Location = new System.Drawing.Point(18, 135);
+            this.Senden.Name = "Senden";
+            this.Senden.Size = new System.Drawing.Size(75, 23);
+            this.Senden.TabIndex = 5;
+            this.Senden.Text = "Senden";
+            this.Senden.UseVisualStyleBackColor = true;
+            this.Senden.Click += new System.EventHandler(this.Senden_Click);
+            // 
+            // AnzahlAufrufe
+            // 
+            this.AnzahlAufrufe.Location = new System.Drawing.Point(15, 109);
+            this.AnzahlAufrufe.Name = "AnzahlAufrufe";
+            this.AnzahlAufrufe.Size = new System.Drawing.Size(100, 20);
+            this.AnzahlAufrufe.TabIndex = 6;
+            // 
+            // Empfangen
+            // 
+            this.Empfangen.Location = new System.Drawing.Point(150, 135);
+            this.Empfangen.Name = "Empfangen";
+            this.Empfangen.Size = new System.Drawing.Size(75, 23);
+            this.Empfangen.TabIndex = 7;
+            this.Empfangen.Text = "Empfangen";
+            this.Empfangen.UseVisualStyleBackColor = true;
+            this.Empfangen.Click += new System.EventHandler(this.Empfangen_Click);
+            // 
+            // client
+            // 
+            this.client.Location = new System.Drawing.Point(150, 65);
+            this.client.Name = "client";
+            this.client.Size = new System.Drawing.Size(100, 20);
+            this.client.TabIndex = 8;
+            this.client.Text = "Client1";
+            // 
+            // clientname
+            // 
+            this.clientname.AutoSize = true;
+            this.clientname.Location = new System.Drawing.Point(147, 48);
+            this.clientname.Name = "clientname";
+            this.clientname.Size = new System.Drawing.Size(64, 13);
+            this.clientname.TabIndex = 9;
+            this.clientname.Text = "Client Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 208);
+            this.Controls.Add(this.clientname);
+            this.Controls.Add(this.client);
+            this.Controls.Add(this.Empfangen);
+            this.Controls.Add(this.AnzahlAufrufe);
+            this.Controls.Add(this.Senden);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MethodenName);
+            this.Controls.Add(this.InterfaceName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InterfaceName;
+        private System.Windows.Forms.TextBox MethodenName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Senden;
+        private System.Windows.Forms.TextBox AnzahlAufrufe;
+        private System.Windows.Forms.Button Empfangen;
+        private System.Windows.Forms.TextBox client;
+        private System.Windows.Forms.Label clientname;
+    }
+}
+
---
+++ HES-Konsole/GUI/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        private ConnectionFactory connectionfactory =  new ConnectionFactory();
+        public bool durable = true;
+        public    bool exclusive = false;
+            public bool autoDelete = false;
+
+public             string serverAddress = "localhost";
+       public      string queuename = "test"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+            connectionfactory.HostName = serverAddress;
+
+            
+
+        }
+
+        private void Senden_Click(object sender, EventArgs e)
+        {
+            send();
+        }
+
+        private void send() {             
+            using (IConnection connection = connectionfactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    // Die Queue wird erzeugt falls sie noch nicht existiert.
+                    var a = channel.QueueDeclare(queuename, durable, exclusive, autoDelete, null);
+                    System.Console.WriteLine("Len: " + a.MessageCount);
+                    System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                    string message = "TestRabbitMQin.NET";
+                    channel.BasicPublish("", queuename, null, encoder.GetBytes(message));
+                    Console.WriteLine("Message Send to RabbitMQ: " + message);
+                }
+            }}
+
+        private void receive()
+        {
+            using (IConnection connection = connectionfactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    var a = channel.QueueDeclare(queuename, durable, exclusive, autoDelete, null);
+                    System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                    QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(queuename, true, consumer);
+                    System.Console.WriteLine("Len: " + a.MessageCount);
+                    BasicDeliverEventArgs ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                    byte[] body = ea.Body;
+                    string message = System.Text.Encoding.UTF8.GetString(body);
+                    System.Console.WriteLine("Message Receive from RabbitMQ: " + message);
+                    System.Console.WriteLine("Len: " + a.MessageCount);
+                    Console.ReadKey();
+
+                }
+            }
+        }
+
+        private void Empfangen_Click(object sender, EventArgs e)
+        {
+            receive();
+        }
+
+    }
+}
